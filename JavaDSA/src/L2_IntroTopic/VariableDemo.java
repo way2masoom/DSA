ae846a61cff860dde360c92e5a22261dfc91c397
way2masoom <way2masoom@gmail.com>
2025-03-18 12:13:40 +0530

    Vatiable Demo

---
+++ JavaDSA/src/L2_IntroTopic/VariableDemo.java
@@ -0,0 +1,18 @@
+package L2_IntroTopic;
+
+import java.security.PublicKey;
+
+public class VariableDemo {
+    public static void main(String[] args){
+        System.out.println("Variable Demo");
+
+        // Assigning value during declaration
+        int speedLimit=80;
+
+        int speed;
+        speed=20;
+        System.out.println("Speed is = "+speed);
+        speed =50;
+        System.out.println("Speed is = "+speed);
+    }
+}
