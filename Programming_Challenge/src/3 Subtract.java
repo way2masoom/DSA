707f869efc91f863045dba63952563ae2a975f0a
way2masoom <way2masoom@gmail.com>
2025-03-30 11:37:20 +0530

    Subtract of two number

--- Programming_Challenge/src/3 Subtract.java
+++ Programming_Challenge/src/3 Subtract.java
@@ -1,5 +1,18 @@
- class Subtract {
-     public static void main(String[] args) {
-         System.out.println("Subtract Calculator");
-     }
+import java.util.Scanner;
+
+class Subtract {
+    public static void main(String[] args) {
+        System.out.println("Subtract Calculator");
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter First number : ");
+        int num1=input.nextInt();
+
+        System.out.print("Enter the Second Number: ");
+        int num2=input.nextInt();
+
+        int subtract=num1-num2;
+
+        System.out.print("Subtract of number is = "+subtract);
+    }
 }
