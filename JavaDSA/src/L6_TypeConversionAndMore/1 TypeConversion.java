23d3bdc65adef7b93d54cd203e2e7fcffa4d61c0
way2masoom <way2masoom@gmail.com>
2025-04-04 23:00:00 +0530

    TypeConversion

---
+++ JavaDSA/src/L6_TypeConversionAndMore/1 TypeConversion.java
@@ -0,0 +1,15 @@
+package L6_TypeConversionAndMore;
+
+class TypeConversion {
+    public static void main(String[] args) {
+        System.out.println("Type Casting");
+
+        // Implicit Conversion
+        float myFloat=5;
+        System.out.println(myFloat);
+
+        // Explicit conversion
+        int myInt=(int)5.0f;
+        System.out.println(myInt);
+    }
+}
