1f483a78d3769a5cb5d668bf22f7a22231d099fb
way2masoom <way2masoom@gmail.com>
2025-04-24 11:56:48 +0530

    LCM of Number

--- Programming_Challenge/src/14 SumOfDigit.java
+++ Programming_Challenge/src/14 SumOfDigit.java
@@ -1,2 +1,29 @@
-public class SumOfDigit {
+import java.util.Scanner;
+
+class SumOfDigit {
+    public static void main(String[] args) {
+        System.out.println("Sum of Digit");
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter the Number Of Digits : ");
+        int digit=input.nextInt();
+
+        int sum=SumOfDigit(digit);
+        System.out.println("Sum of Digit "+digit+" is = "+sum);
+    }
+
+    // Method to find the sum of the digit
+    public static int SumOfDigit(int num){
+        int sum=0;
+        int i=0;
+        while (num>0){
+            //lastNumber=num%10;
+            //sum=sum+lastNumber;
+
+            sum+=num%10; // sum= sum+(num%10) Sum and the last number is added
+            num/=10;
+            i++;
+        }
+        return sum;
+    }
 }
---
+++ Programming_Challenge/src/15 LCM.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class LCM {
+    public static void main(String[] args) {
+        System.out.println("LCM Lowest Common Multiple");
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter first Number : ");
+        int a=input.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b=input.nextInt();
+
+        int lcm=LCMOfNumber(a,b);
+        System.out.println("LCM of "+a+" and "+b+" is = "+lcm);
+    }
+
+    // Function to find the Lowest Common Multiple
+    public static int LCMOfNumber(int first,int second){
+        int i=1;
+        // loop will run till the Multiple of Both number
+        while (true){
+            int factor=first*i;
+            if (factor%second==0){
+                return factor; // Return the LCM
+            }
+            i++;
+        }
+    }
+}
