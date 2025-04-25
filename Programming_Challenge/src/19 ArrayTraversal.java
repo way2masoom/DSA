f9731e2e85a7b10ed2016c17de8bdc4400a9f728
way2masoom <way2masoom@gmail.com>
2025-04-25 22:43:13 +0530

    Araay Searching

--- Programming_Challenge/src/19 ArrayTraversal.java
+++ Programming_Challenge/src/19 ArrayTraversal.java
@@ -1,2 +1,13 @@
-public class ArrayTraversal {
+class ArrayTraversal {
+    public static void main(String[] args) {
+        System.out.println("Array Traversal");
+        //Declaring a Array
+        int[] arr={10,20,30,40,50,60};
+
+        System.out.println("Displaying the Array");
+        for (int i=0;i< arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
 }
