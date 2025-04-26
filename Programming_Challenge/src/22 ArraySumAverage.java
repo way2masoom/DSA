989a611f12014205475329a84f103c1025710c66
way2masoom <way2masoom@gmail.com>
2025-04-26 10:44:46 +0530

    Modify array programs

---
+++ Programming_Challenge/src/22 ArraySumAverage.java
@@ -0,0 +1,11 @@
+// Create Sum and Average of All Element in an array
+class ArraySumAverage {
+    public static void main(String[] args) {
+        System.out.println("Array sum and Average");
+
+        // Taking array Input from user
+        int[] numArray=ArrayUtility.inputArray();
+
+        ArrayUtility.printArray(numArray);
+    }
+}
