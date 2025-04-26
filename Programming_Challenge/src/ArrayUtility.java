60e8aa84efe439abf7ae07eba03c924104c51ac7
way2masoom <way2masoom@gmail.com>
2025-04-26 10:47:20 +0530

    Modified

--- Programming_Challenge/src/ArrayUtility.java
+++ Programming_Challenge/src/ArrayUtility.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class ArrayUtility {
+    // Method to input an integer array from the user
+    public static int[] inputArray() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter the Size Array: ");
+        int size = input.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Please Enter the Number of Elements ");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = input.nextInt();
+        }
+        return arr;
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr){
+        System.out.print("Array Elements : ");
+        for(int num:arr){
+            System.out.print(+num +" " );
+        }
+        System.out.println();
+    }
 }
