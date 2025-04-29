66e804648a54d421e478d57baac2e81d1676cc80
way2masoom <way2masoom@gmail.com>
2025-04-29 12:59:59 +0530

    array is sorted or not

---
+++ Programming_Challenge/src/25 IsSortedArray.java
@@ -0,0 +1,42 @@
+class IsShortedArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Check Point of Array is shorted or Not");
+
+        // Taking array as input from user
+        int[] numArray=ArrayUtility.inputArray();
+
+        boolean isInc=IsIncreasing(numArray);
+        boolean isDec=IsDecreasing(numArray);
+
+        // Check Point is the array is shorted or not
+        if(isInc || isDec){
+            System.out.println("Array is Shorted");
+        }else {
+            System.out.println("Array is not Shorted");
+        }
+    }
+
+    // Check if the array is in Increasing Order
+    public static boolean IsIncreasing(int[] numArray){
+        int i=1;
+        while (i<numArray.length){
+            if(numArray[i]<numArray[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+    // Check if the array is in Decreasing Order
+    public static boolean IsDecreasing(int[] numArray){
+        int i=1;
+        while (i<numArray.length){
+            if(numArray[i]>numArray[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
