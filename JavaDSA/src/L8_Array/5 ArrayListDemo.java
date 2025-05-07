deb0fc0bc02d9ea1f1a8409c765a11a791ac54aa
way2masoom <way2masoom@gmail.com>
2025-05-07 10:18:27 +0530

    Array List Demo

--- JavaDSA/src/L8_Array/5 ArrayListDemo.java
+++ JavaDSA/src/L8_Array/5 ArrayListDemo.java
@@ -1,4 +1,36 @@
 package L8_Array;
+import java.util.ArrayList;
+class ArrayListDemo {
+    public static void main(String[] args) {
+        System.out.println("Demo of Array List");
 
-public class ArrayListDemo {
+        // Syntax : ArrayList<Type> arrayList= new ArrayList<>();
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        ArrayList<String> language=new ArrayList<>();
+
+        // adding String Element to the array(language)
+        language.add("Java");
+        language.add("JavaScript");
+        language.add("C++");
+        System.out.println(language);
+
+        // Accessing String Element of the arrayList(language)
+        String str = language.get(1);
+        System.out.println(str);
+
+        // removing String Element from the array(language)
+        language.remove(1);
+        System.out.println(language);
+
+        language.add("SQL");
+        language.add("Go");
+        System.out.println(language);
+
+        // Changing the String Element of the Array(Language)
+        language.set(3,"Swift");
+        System.out.println(language);
+
+
+
+    }
 }
