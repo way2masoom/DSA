46f3adf73f7c53729bea717b0cf2396a7388c365
way2masoom <way2masoom@gmail.com>
2025-05-07 10:19:00 +0530

    Array Reverse

--- Programming_Challenge/src/27 ArrayReverse.java
+++ Programming_Challenge/src/27 ArrayReverse.java
@@ -1,2 +1,29 @@
-public class ArrayReverse {
+class ArrayReverse {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Array Swap Program");
+
+        // Taking Array Input
+        int[] numArray=ArrayUtility.inputArray();
+
+        System.out.println("original Array");
+        ArrayUtility.printArray(numArray);
+
+        System.out.println();
+
+        reverse(numArray);
+        System.out.println("Reversed Array:");
+        ArrayUtility.printArray(numArray);
+    }
+
+    // Method to reverse the Array
+    public static void reverse(int[] arr){
+        int n=arr.length;
+        int mid=n/2;
+        for (int i = 0; i < mid; i++) {
+            // Swap element at index i with the element at the mirrored position from end
+            int swap=arr[i];
+            arr[i]=arr[(arr.length-1)-i];
+            arr[(arr.length-1)-i]=swap;
+        }
+    }
 }
