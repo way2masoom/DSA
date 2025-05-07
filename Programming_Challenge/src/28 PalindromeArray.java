a0883df60c8d58bd4e29e0268676a4a293321705
way2masoom <way2masoom@gmail.com>
2025-05-07 19:35:11 +0530

    Palindrom array no change

--- Programming_Challenge/src/28 PalindromeArray.java
+++ Programming_Challenge/src/28 PalindromeArray.java
@@ -1,2 +1,31 @@
-public class PalindromeArray {
-}
+class PalindromeArray {
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker \n");
+
+        // taking array as input
+        int[] numArray=ArrayUtility.inputArray();
+
+        // calling the Method
+        boolean palindromeArray=isPalindrome(numArray);
+
+        if(palindromeArray){
+            System.out.println("Your array is Palindrome");
+        }else {
+            System.out.println("Your array is not Palindrome");
+        }
+
+    }
+
+        // Method to check the array is Palindrome or not
+        public static boolean isPalindrome(int[] arr){
+            int i=0;
+            while (i< arr.length){
+                //Condition: Compare the current element with its corresponding element from the end
+                if (arr[i]!=arr[(arr.length-1)-i]){
+                    return false; //  // If not equal, it's not a palindrome
+                }
+                i++;
+            }
+            return true;
+        }
+    }
---
+++ Programming_Challenge/src/29 MergeSortedArray.java
@@ -0,0 +1,14 @@
+class mergeArray {
+    public static void main(String[] args) {
+        System.out.println("Program to Merge two shorted Array");
+
+        //Taking input as arrya
+        int[] numArray=ArrayUtility.inputArray();
+
+    }
+
+    // Method to merge to shorted array
+    public static int mergeSortedArray(int[] arr1,int[] arr2){
+        
+    }
+}
