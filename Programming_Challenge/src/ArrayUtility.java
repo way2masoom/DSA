b9a3022475ef4284bbe678422ee9349d41c32e2b
way2masoom <way2masoom@gmail.com>
2025-05-12 07:28:14 +0530

    Added 2D Array Input

--- Programming_Challenge/src/ArrayUtility.java
+++ Programming_Challenge/src/ArrayUtility.java
@@ -17,11 +17,34 @@ public class ArrayUtility {
         return arr;
     }
 
+    // Method to input an integer 2D array from the user
+    public static int[][] input2DArray() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter the Number of Rows : ");
+        int rows = input.nextInt();
+
+        System.out.print("Please Enter the Number of Columns : ");
+        int columns = input.nextInt();
+
+        int[][] arr = new int[rows][columns];
+
+        // Outer loop for Rows
+        for (int i = 0; i < rows; i++) {
+            // Inner Loop for columns
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter value at Row " + (i + 1) + ", Column " + (j + 1) + ": ");
+                arr[i][j] = input.nextInt();
+            }
+        }
+        return arr;
+    }
+
     // Method to print the array
-    public static void printArray(int[] arr){
+    public static void printArray(int[] arr) {
         System.out.print("Array Elements : ");
-        for(int num:arr){
-            System.out.print(+num +" " );
+        for (int num : arr) {
+            System.out.print(+num + " ");
         }
         System.out.println();
     }
