cdb5c09fa8b7ff56cb6618b0e3ffc23b4c233113
way2masoom <way2masoom@gmail.com>
2025-12-04 08:22:19 +0545

    Let's go again

--- JavaDSA/src/L1_Basic/FirstProgram.java
+++ JavaDSA/src/L1_Basic/FirstProgram.java
@@ -3,5 +3,6 @@ package L1_Basic;
 public class FirstProgram {
     public static void main(String[] args){
         System.out.println("This is first Program");
+        System.out.println("Let's Learn one more time");
     }
 }
