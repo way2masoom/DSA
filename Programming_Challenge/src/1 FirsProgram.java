6ab36583885465b12902caed5f1b6d7facd70f9c
way2masoom <way2masoom@gmail.com>
2025-12-04 08:42:03 +0545

    First Program

--- Programming_Challenge/src/1 FirsProgram.java
+++
@@ -1,5 +0,0 @@
-class FirsProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ Programming_Challenge/src/1 FirstProgram.java
@@ -0,0 +1,32 @@
+class FirstProgram{
+    public static void main(String[] args){
+        System.out.println("Let's Go Again");
+        System.out.println("Good morning");
+
+        System.out.println();
+        System.out.println("Left Pyramid");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+        System.out.println("Reverse Right Pyramid");
+        System.out.println("* * * * *");
+        System.out.println("* * * * ");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.println("* ");
+
+
+        System.out.println();
+        System.out.println("Left half Pyramid");
+        System.out.println("       *");
+        System.out.println("     * *");
+        System.out.println("   * * *");
+        System.out.println(" * * * *");
+        System.out.println("* * * * *");
+    }
+
+}
