6b5d057758a0960234bb1847cd77bebe736a3c61
way2masoom <way2masoom@gmail.com>
2025-12-04 11:21:27 +0545

    Sum of two number

--- Programming_Challenge/src/2 Sum.java
+++ Programming_Challenge/src/2 Sum.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 class Sum {
     public static void main(String[] args){
-      System.out.println("Sum calculator ");
-//      for Input
+        System.out.println("Sum calculator");
         Scanner input=new Scanner(System.in);
 
-        System.out.print("Enter the first Number : ");
+        System.out.print("Enter Your first number : ");
         int num1=input.nextInt();
 
-        System.out.print("Enter the second number : ");
+        System.out.print("Enter Your Second number : ");
         int num2=input.nextInt();
 
         int sum=num1+num2;
-        System.out.println("Sum of two number is = "+sum);
-
+       System.out.print("Sum of number is = "+sum);
     }
 }
