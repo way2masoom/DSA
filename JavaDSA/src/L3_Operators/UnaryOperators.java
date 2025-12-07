018be9981cc512a92fa8db06d07a8c0575d25893
way2masoom <way2masoom@gmail.com>
2025-12-07 11:09:06 +0545

    Unery Operators

--- JavaDSA/src/L3_Operators/UnaryOperators.java
+++ JavaDSA/src/L3_Operators/UnaryOperators.java
@@ -6,28 +6,30 @@ public class UnaryOperators {
         System.out.println("Unary Operators");
 
         int a=10,b=20;
-        System.out.println("A = "+a + " B = "+b);
-       int res1,res2;
+        System.out.println("value of A = "+a + " value of B = "+b);
+
 
        //Increment Operator
-       res1=++a;
-        System.out.println("A ="+res1);
+        int inc=++a;
+        System.out.println("Value of A After increment  ="+inc);
 
         //Decrement Operator
-        res2=--b;
-        System.out.println("B ="+res2);
+        int dec=--b;
+        System.out.println("Value of B after Decrement  ="+dec);
 
         // Different between a++ and ++a
         int var1=10 ,var2=20;
 
+        System.out.println();
         // If I used a++ then the original value of var will return first and var will increase by 1
         // If I used ++a then the original values of var will increase by 1 and then return
-        System.out.println(var1++);
-        System.out.println(++var1);
+        System.out.println("Var1 = "+var1 +" Var2 = "+var2+ "\n");
+        System.out.println("Pree Increment var1 = "+ ++var1);
+        System.out.println("Pree Decrement var1= "+ --var1 +"\n");
 
         System.out.println();
 
-        System.out.println(++var2);
-        System.out.println(var2++);
+        System.out.println("Post Increment var2 = "+ var2++);
+        System.out.println("Post Decrement var2= "+var2--);
     }
 }
