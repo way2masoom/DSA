f464e27011346439bf53dcd6b1d1916ef0b90a85
way2masoom <way2masoom@gmail.com>
2025-12-07 13:04:33 +0545

    Area of triangle

--- Programming_Challenge/src/11 A AreaOfTriangle.java
+++ Programming_Challenge/src/11 A AreaOfTriangle.java
@@ -1,2 +1,24 @@
-public class AreaOfTriangle {
+import java.util.Scanner;
+
+class AreaOfTriangle {
+    public static void main(String[] args) {
+        System.out.println("Area of Triangle");
+        // formula of are of Triangle is A=1/2(B*H) OR A= (breath * height)/2
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Breath of Triangle : ");
+        double breath=input.nextDouble();
+
+        System.out.print("Enter the Height of Triangle : ");
+        double height=input.nextDouble();
+
+        // Logic For Area of Triangle
+        double area=0.5f*(breath*height);
+
+        // we can also Use this logic
+        //double area = (base * height) / 2;
+
+
+        System.out.println("Area of Triangle is = "+area);
+    }
 }
