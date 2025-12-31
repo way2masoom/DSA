43fb3c7c5578a3ec2e86b1e4a9f663c93f4fe096
way2masoom <way2masoom@gmail.com>
2025-12-31 11:10:46 +0545

    Array Demo

--- JavaDSA/src/L8_Array/1 ArrayDemo.java
+++ JavaDSA/src/L8_Array/1 ArrayDemo.java
@@ -11,17 +11,17 @@ class ArrayDemo {
         myArr[4]=55;
         myArr[3]=21;
 
-        System.out.println(myArr[3]);
-        System.out.println(myArr);
-        System.out.println(myArr.length);
+        System.out.println("Index of 3 = "+myArr[3]);
+        System.out.println("Array Length = "+myArr.length);
 
+        System.out.println("Printing all the array elements");
         for (int i=0;i<myArr.length;i++){
             System.out.println(myArr[i]);
         }
 
         System.out.println("Printing in reverse");
-        for(int i= myArr.length-1;i>=0;i--){
-            System.out.print(" "+myArr[i]);
+        for (int i=myArr.length-1;i>=0;i--){
+            System.out.println(myArr[i]);
         }
 
     }
