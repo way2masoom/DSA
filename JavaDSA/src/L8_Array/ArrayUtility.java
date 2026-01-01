3e70ee7485073603de5f005d778622af976e0503
way2masoom <way2masoom@gmail.com>
2026-01-01 08:56:48 +0545

    Array utility

---
+++ JavaDSA/src/L8_Array/ArrayUtility.java
@@ -0,0 +1,24 @@
+package L8_Array;
+
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        // Asking user for number of elements
+        System.out.print("Enter the number of elements you want in the array: ");
+        int size=input.nextInt();
+
+        // Creating array of given size
+        int[] nums=new int[size];
+
+        // Loop to take array elements from user
+        int i=0;
+        while (i<size){
+            System.out.print("Please Enter the Element No "+(i+1) + " : ");
+            nums[i] =input.nextInt();
+            i++;
+        }
+    }
+}
