99664d0dbde41d6cc9f31163281747f3f0b3d1ea
way2masoom <way2masoom@gmail.com>
2026-01-06 09:25:56 +0545

    Fixed 2D array search

--- JavaDSA/src/L8_Array/ProgrammingChallanges/9 Search2DArray.java
+++ JavaDSA/src/L8_Array/ProgrammingChallanges/9 Search2DArray.java
@@ -5,26 +5,34 @@ import java.util.Scanner;
 class Search2DArray {
     public static void main(String[] args) {
         System.out.println("---- Search In 2DArray ----\n");
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int[][] numArray=ArrayUtility.input2DArray();
+        int[][] numArray = ArrayUtility.input2DArray();
 
         System.out.print("Enter the Number you want to search : ");
-        int num=input.nextInt();
+        int num = input.nextInt();
 
+        boolean isFound = isNumFound(numArray, num);
+
+        if (isFound) {
+            System.out.println("Your number Found in the array" + num);
+        } else {
+            System.out.println("Your number NOT Found in the array" + num);
+        }
     }
 
-    public static boolean isNumFound(int[][] numArray,int num){
-
-        int i=0;
-        while (i<numArray.length){
-            if(numArray[i]==num){
-                return true;
-            }else {
-                return false;
+    public static boolean isNumFound(int[][] numArray, int num) {
+        int i = 0;
+        while (i < numArray.length) {
+            int j = 0;
+            while (j < numArray[i].length) {
+                if (numArray[i][j] == num) {
+                    return true;
+                }
+                j++;
             }
             i++;
         }
-
+        return false;
     }
 }
